2f59d6db0f8015d4dd6412dc0c8af79b7b0b266f
Denis8Bits <dmorales-2024433@kinal.edu.gt>
2025-09-05 16:29:12 -0600

    Subida de proyecto

---
+++ src/main/java/org/datacoins/ventalibros/VentalibrosApplication.java
@@ -0,0 +1,13 @@
+package org.datacoins.ventalibros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VentalibrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VentalibrosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/datacoins/ventalibros/VentalibrosApplicationTests.java
@@ -0,0 +1,13 @@
+package org.datacoins.ventalibros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VentalibrosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
